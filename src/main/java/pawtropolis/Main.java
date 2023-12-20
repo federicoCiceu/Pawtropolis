7f656b12f164ab727c5af22245de33e4a8fa3674
Aliscice <alicecintio0199@gmail.com>
2023-12-20 09:50:04 +0100

    Update Main.java

---
+++ src/main/java/pawtropolis/Main.java
@@ -0,0 +1,19 @@
+package pawtropolis;
+
+import pawtropolis.game.Room;
+import pawtropolis.game.Player;
+import pawtropolis.game.gamecontroller.VideoGameController;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Room currentRoom = VideoGameController.roomMonstadt;
+        Player player = new Player("Alex", 100, currentRoom);
+
+        VideoGameController videoGameController = new VideoGameController(player);
+
+        videoGameController.startGame();
+    }
+
+}
