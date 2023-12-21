c7fb56926634e6c9780a55a676dcd9531e3eb0e7
Aliscice <alicecintio0199@gmail.com>
2023-12-21 15:20:45 +0100

    Update DropCommandAction.java

---
+++ src/main/java/pawtropolis/game/commands/DropCommandAction.java
@@ -0,0 +1,42 @@
+package pawtropolis.game.commands;
+
+import pawtropolis.game.Item;
+import pawtropolis.game.Player;
+import pawtropolis.game.PopulateGame;
+import pawtropolis.game.gamecontroller.CommandController;
+
+import java.util.Optional;
+
+public class DropCommandAction implements CommandController {
+    private final Player player;
+    private final PopulateGame populateGame;
+
+    public DropCommandAction(Player player, PopulateGame populateGame) {
+        this.player  = player;
+        this.populateGame = populateGame;
+    }
+
+    public void dropItem(String itemName) {
+        Optional<Item> optionalItem = player.getItems()
+                .stream()
+                .filter(item -> item.getName().equals(itemName))
+                .findFirst();
+
+        if (optionalItem.isPresent()) {
+            Item item = optionalItem.get();
+            player.dropItem(item);
+            populateGame.getCurrentRoom().addItem(item);
+        } else {
+            System.out.println("Item not found in the current room");
+        }
+    }
+
+    @Override
+    public void execute(String[] inputParts) {
+        if (inputParts.length == 2) {
+            dropItem(inputParts[1]);
+        } else {
+            System.out.println("Please choose the item to drop.");
+        }
+    }
+}
