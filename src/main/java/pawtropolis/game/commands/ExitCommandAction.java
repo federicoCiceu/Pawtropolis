4a041c09c71f15553b64f805ee83b5ffe6f75c2f
Aliscice <alicecintio0199@gmail.com>
2024-01-05 10:13:14 +0100

    Update ExitCommandAction.java

---
+++ src/main/java/pawtropolis/game/commands/ExitCommandAction.java
@@ -0,0 +1,11 @@
+package pawtropolis.game.commands;
+
+import pawtropolis.game.gamecontroller.CommandController;
+
+public class ExitCommandAction  implements CommandController {
+
+    @Override
+    public void execute(String[] inputParts) {
+        System.out.println("Pawtropolis hopes to see you again!");
+    }
+}
